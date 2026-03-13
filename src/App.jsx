aaf85b011dea70bc8c6d094f93ae95c1cc30aa7f
federicocano8-art <federicocano8@gmail.com>
2026-03-13 00:09:16 -0300

    Implement routing with React Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Inventario from "./pages/Inventario";
+import Moviles from "./pages/Moviles";
+import ERAs from "./pages/ERAs";
+
 export default function App() {
   return (
-    <div>
-      <h1>Sistema Bomberos Ramallo</h1>
-      <p>La aplicación está funcionando.</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/inventario" element={<Inventario />} />
+        <Route path="/moviles" element={<Moviles />} />
+        <Route path="/eras" element={<ERAs />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
